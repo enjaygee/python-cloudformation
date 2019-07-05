d42ef22751d98a010f8c06cedef93d250939df37
Gompper, Nick <14997@icf.com>
2019-07-05 15:36:51 +0000

    add comments; upgrade pip

--- init.sh
+++ init.sh
@@ -11,7 +11,9 @@ export AWS_DEFAULT_REGION="us-east-1"
 
 # Open python virtual environment in directory ".venv".  See https://docs.python.org/3/library/venv.html
 python3 -m venv .venv
+# activate the virtual environment by sourcing the associated bin/activate file. See https://docs.python.org/3/tutorial/venv.html
 . .venv/bin/activate
+pip install --upgrade pip
 pip install -r requirements.txt
 
 python simple-infrastructure.py $1
