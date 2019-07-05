552beeefff5a3dd455b2ae4290f788087e7a20bf
Gompper, Nick <14997@icf.com>
2019-07-05 16:51:35 +0000

    removing git api key

--- init.sh
+++ init.sh
@@ -1,12 +1,5 @@
 #!/bin/bash -e
 
-if [ -z "$1" ]
-  then
-    echo "Please provide github api access token .
-        ./init.sh <<github apip access token>> "
-    exit 1
-fi
-
 export AWS_DEFAULT_REGION="us-east-2"
 
 # Open python virtual environment in directory ".venv".  See https://docs.python.org/3/library/venv.html
@@ -16,4 +9,4 @@ python3 -m venv .venv
 pip install --upgrade pip
 pip install -r requirements.txt
 
-python simple-infrastructure.py $1
+python simple-infrastructure.py
